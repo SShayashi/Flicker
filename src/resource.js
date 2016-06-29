45ac0314c87d470459bbddf859dad8de5a93cb6d
林 真史 <sshayashi0208@gmail.com>
2016-06-29 16:10:42 +0900

    resouceの追加

--- src/resource.js
+++ src/resource.js
@@ -2,7 +2,10 @@ var res = {
     background :"res/image/background.png",
     player: "res/image/player.png",
     crash_star:"res/particle/pop_star.plist",
-    p_star:"res/particle/star.png"
+    p_star:"res/particle/star.png",
+    pop_coin:"res/particle/pop_coin.plist",
+    p_coin:"res/particle/pop_coin.png",
+    StatusLayer_json:"res/StatusLayer-web.json"
 };
 var g_resources = [];
 for (var i in res) {
